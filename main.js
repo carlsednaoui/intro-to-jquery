f1ead3249393874fdaf96dc0bcefadfab574cb06
carl sednaoui <carlsednaoui@gmail.com>
2013-11-14 00:13:18 -0500

    Cleans up hadouken, this code needs to be cleaned up

--- main.js
+++ main.js
@@ -12,9 +12,10 @@ $( document ).ready(function() {
   // Main example
   $('#main-example').hover(function() {this.src = 'images/ruy_animated.gif'}, function() {this.src = 'images/ruy_stand_still.png'});
   $('#main-example').mousedown(function() {this.src = 'images/ryu_hadoken_pose.png'});
+  $('#main-example').mousedown(function() {$('.moving-hadouken').remove();});
   $('#main-example').mousedown(function() {$('#main-example-container').append('<img style="vertical-align: top; margin-top: 70px;" src="images/hadouken.gif" class="moving-hadouken">');});
   $('#main-example').mousedown(function() {
-    $('.moving-hadouken').animate({"margin-left": "500px"}, 400, 'swing', function() {
+    $('.moving-hadouken').animate({"margin-left": "600px"}, 1000, 'swing', function() {
       this.remove();
     });
   });
