6492e084e2b66b8e012615a7fc90b7e2729d62aa
carl sednaoui <carlsednaoui@gmail.com>
2013-11-14 22:03:31 -0500

    Adds basic example

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ Add google authorship
 Add parallax events
 Add my twitter handle
 Show open sourced code
+
+# next guide
+http://net.tutsplus.com/tutorials/html-css-techniques/the-30-css-selectors-you-must-memorize/
--- main.js
+++ main.js
@@ -9,22 +9,15 @@
 
 
 $( document ).ready(function() {
-  // Main example
-  $('#main-example').hover(function() {this.src = 'images/ruy_animated.gif'}, function() {this.src = 'images/ruy_stand_still.png'});
+  // jQuery project demo
+  $('#main-example').hover(function() {this.src = 'images/ryu_animated.gif'}, function() {this.src = 'images/ryu_stand_still.png'});
   $('#main-example').mousedown(function() {this.src = 'images/ryu_hadoken_pose.png'});
-  // $('#main-example').mousedown(function() {$('.moving-hadouken').remove();});
-  $('#main-example').mousedown(function() {$('#main-example-container').append('<img style="vertical-align: top; margin-top: 70px; margin-left: -100px;" src="images/hadouken.gif" class="moving-hadouken">');});
+  $('#main-example').mousedown(function() {$('.moving-hadouken').remove();});
+  $('#main-example').mousedown(function() {$('#main-example-container').append('<img class="demo-hadouken moving-hadouken" src="images/hadouken.gif" class="moving-hadouken">');});
   $('#main-example').mousedown(function() {
     $('.moving-hadouken').animate({"margin-left": "600px"}, 1000, 'swing', function() {
       this.remove();
     });
   });
-  $('#main-example').mouseup(function() {this.src = 'images/ruy_animated.gif'});
-  
-
-
-  // .animate( properties [, duration ] [, easing ] [, complete ] )
-
-
-
+  $('#main-example').mouseup(function() {this.src = 'images/ryu_animated.gif'});
 });
