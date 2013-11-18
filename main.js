8c716dc906441b3bb7b4b7d66b4c130301a2d517
carl sednaoui <carlsednaoui@gmail.com>
2013-11-17 23:07:19 -0500

    Finishes first part of project

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 $( document ).ready(function() {
+  // Green box positioning logic
   $(window).on('scroll', function() {
     var y_scroll_pos   = window.pageYOffset,
         greenBox       = $('#green-box-container').position().top,
@@ -13,6 +14,23 @@ $( document ).ready(function() {
 
     if (y_scroll_pos > greenBoxBottom)
         $('#green-box-container').css({'position': 'absolute'});
+  });
+
+  // Code along positioning logic
+  $(window).on('scroll', function() {
+    var y_scroll_pos       = window.pageYOffset,
+        divContainer       = $('#my-div-container').position().top,
+        divContainerTop    = $('#stop-div-container-top').position().top + 100,
+        divContainerBottom = $('#stop-div-container-bottom').position().top - 220;
+
+    if (y_scroll_pos > divContainer)
+        $('#my-div-container').css({'position': 'fixed', 'top': '10px'});
+
+    if (y_scroll_pos < divContainerTop)
+        $('#my-div-container').css({'position': 'inherit'});
+
+    if (y_scroll_pos > divContainerBottom)
+        $('#my-div-container').css({'position': 'absolute'});
   });
 
   
