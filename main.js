17169612c9adbb9ecf126d8b020f2853cae04e74
carl sednaoui <carlsednaoui@gmail.com>
2013-11-17 19:33:42 -0500

    Adds waypoints, thanks slippy

--- main.js
+++ main.js
@@ -1,42 +1,31 @@
 $( document ).ready(function() {
   // Green box logic
-  $(window).on('scroll', function() {
-      var y_scroll_pos = window.pageYOffset;
-      var scroll_pos_test = $('#green-box-container').position().top;
-
-      if(y_scroll_pos > scroll_pos_test) {
-          $('#green-box-container').css({'position': 'fixed', 'top': '10px'});
-      }
+  $('#green-box-container').waypoint(function(direction) {
+    if (direction == 'down') {
+      $('#green-box-container').css({'position': 'fixed', 'top': '10px'});
+    } else {
+      $('#green-box-container').css({'position': 'inherit'});
+    }
   });
 
-  $(window).on('scroll', function() {
-      var y_scroll_pos = window.pageYOffset;
-      var scroll_pos_test = $('#stop-green-top').position().top + 100;
-
-      if(y_scroll_pos < scroll_pos_test) {
-          $('#green-box-container').css({'position': 'inherit'});
-      }
-  });
-
-  $(window).on('scroll', function() {
-      var y_scroll_pos = window.pageYOffset;
-      var scroll_pos_test = $('#stop-green-bottom').position().top - 220;
-
-      if(y_scroll_pos > scroll_pos_test) {
-          $('#green-box-container').css({'position': 'absolute'});
-      }
-  });
+  $('#stop-green-bottom').waypoint(function(direction) {
+    if (direction == 'down') {
+      $('#green-box-container').css({'position': 'absolute'});
+    } else {
+      $('#green-box-container').css({'position': 'fixed', 'top': '10px'});
+    }
+  }, { offset: 220 });
 
 
   // jQuery project demo
   $('#main-example').hover(function() {this.src = 'images/ryu_animated.gif'}, function() {this.src = 'images/ryu_stand_still.png'});
-  $('#main-example').mousedown(function() {this.src = 'images/ryu_hadoken_pose.png'});
-  $('#main-example').mousedown(function() {$('.moving-hadouken').remove();});
-  $('#main-example').mousedown(function() {$('#main-example-container').append('<img class="demo-hadouken moving-hadouken" src="images/hadouken.gif" class="moving-hadouken">');});
-  $('#main-example').mousedown(function() {
-    $('.moving-hadouken').animate({"margin-left": "600px"}, 1000, 'swing', function() {
+  $('#main-example').on('mousedown', function() {this.src = 'images/ryu_hadoken_pose.png'});
+  $('#main-example').on('mousedown', function() {$('.moving-hadouken').remove();});
+  $('#main-example').on('mousedown', function() {$('#main-example-container').append('<img class="demo-hadouken moving-hadouken" src="images/hadouken.gif" class="moving-hadouken">');});
+  $('#main-example').on('mousedown', function() {
+    $('.moving-hadouken').animate({"margin-left": "200px"}, 1000, 'swing', function() {
       this.remove();
     });
   });
-  $('#main-example').mouseup(function() {this.src = 'images/ryu_animated.gif'});
+  $('#main-example').on('mouseup', function() {this.src = 'images/ryu_animated.gif'});
 });
